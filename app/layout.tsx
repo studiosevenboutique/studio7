9368bc0da629eaa1b6bbdd42321daa75024e1816
wmaxb1992 <66269844+wmaxb1992@users.noreply.github.com>
2026-01-15 16:48:37 -0800

    Add Klaviyo tracking scripts to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -154,6 +154,18 @@ export default function RootLayout({
             t.parentNode.insertBefore(e,t)}();
           `}
         </Script>
+
+        {/* Klaviyo Tracking Script */}
+        <Script
+          async
+          src="https://static.klaviyo.com/onsite/js/YtmLE9/klaviyo.js?company_id=YtmLE9"
+          strategy="afterInteractive"
+        />
+        <Script id="klaviyo-init" strategy="afterInteractive">
+          {`
+            !function(){if(!window.klaviyo){window._klOnsite=window._klOnsite||[];try{window.klaviyo=new Proxy({},{get:function(n,i){return"push"===i?function(){var n;(n=window._klOnsite).push.apply(n,arguments)}:function(){for(var n=arguments.length,o=new Array(n),w=0;w<n;w++)o[w]=arguments[w];var t="function"==typeof o[o.length-1]?o.pop():void 0,e=new Promise((function(n){window._klOnsite.push([i].concat(o,[function(i){t&&t(i),n(i)}]))}));return e}}})}catch(n){window.klaviyo=window.klaviyo||[],window.klaviyo.push=function(){var n;(n=window._klOnsite).push.apply(n,arguments)}}}}();
+          `}
+        </Script>
       </body>
     </html>
   )
